ac131d181918811aebe10ee8025a20e4f95cc1da
Brayan Frank <brayanfrank.sm@gmail.com>
2022-04-30 14:59:19 -0300

    implementing cokkies

--- iRock/index.js
+++ iRock/index.js
@@ -1,17 +1,17 @@
 let isHappy = false
-let myName
+let myName = readCookie("username") || undefined
 let sadCounter = 0;
 let isAngry = false;
 
-setInterval("withoutAnyInteraction()", 18000) 
-setTimeout("greetings()", 1000)
+setInterval("withoutInteraction()", 18000)
+setTimeout("greetings()", 100)
 
-function withoutAnyInteraction() {
+function withoutInteraction() {
   if (!isHappy) {
     if (!isAngry && sadCounter < 3) {
       window.alert(`Oi, alguém por aí?`)
       sadCounter++
-      if (sadCounter > 2) {
+      if (sadCounter == 3) {
         setIRockImage("./img/irock-sad.png")
       }
     }
@@ -29,7 +29,7 @@ function greetings() {
 
 function touchRock() {
 
-  if(isAngry) {
+  if (isAngry) {
     isAngryPhases()
     return;
   }
@@ -38,6 +38,7 @@ function touchRock() {
 
   if (!isHappy && !myName) {
     myName = window.prompt('Olá, finalmente alguém. Qual é o seu nome?')
+    writeCookie("username", myName, 2)
     if (!myName) {
       return
     }
@@ -69,11 +70,11 @@ function happyIRock() {
   const timer = setTimeout(() => {
     setIRockImage('./img/irock.png');
     isHappy = false
-  }, 9000) 
+  }, 9000)
 }
 
 function isAngryPhases() {
-  switch (sadCounter){
+  switch (sadCounter) {
     case 3:
       window.alert(`Não quero conversar com você! Você me abandonou!`)
       sadCounter--;
@@ -93,6 +94,37 @@ function isAngryPhases() {
 
 function resizeIRock() {
   let irock = document.getElementById('rockImg');
-  
- irock.style.height = `50%`
+
+  irock.style.height = `50%`
+}
+
+//Cookies
+
+function writeCookie(name, value, days) {
+  let expires = ""
+
+  if (days) {
+    let date = new Date()
+
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 10000))
+    expires = `; expires=${date.toUTCString()}`
+  }
+
+  document.cookie = `${name}=${value}${expires}; path=/`
+}
+
+function readCookie(name) {
+  let searchName = name + '='
+  let cookies = document.cookie.split(';')
+  let selectedCookie = cookies.filter(cookie => cookie.trim().includes(searchName))
+
+  if (selectedCookie.length == 1) {
+    let [key, value] = selectedCookie[0].split('=')
+    return value;
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  writeCookie(name, "", -1)
 }
