8e757ee24557d38c2e9961dd200232b6af2748ae
Brayan Frank <brayanfrank.sm@gmail.com>
2022-11-20 22:46:41 -0300

    initializing project and creating story

---
+++ stickAdventure/src/index.js
@@ -0,0 +1,15 @@
+function loadScene() {
+    
+}
+
+function changeScene() {
+
+    const sceneBackground = document.getElementById('scene').style.background
+
+    switch (curScene) {
+        case 1:
+           sceneBackground = ''
+    }
+}
+
+
