9727abc46c55100d298c9aebdd20dd942264ce25
Brayan Frank <brayanfrank.sm@gmail.com>
2022-12-12 19:16:49 -0300

    the adventure final version

--- theAdventure/src/assets/scenes.js
+++ theAdventure/src/assets/scenes.js
@@ -1,7 +1,7 @@
 const scenes = {
     1: {
         "text": "É hora de começar sua jornada como aventureiro, está pronto?",
-        "images": ["./assets/images/scene01.jpg"],
+        "image": "./assets/images/scene01.jpg",
         "options": [
             {
                 "text": "Pode apostar!",
@@ -14,8 +14,8 @@ const scenes = {
         ]
     },
     2: {
-        "text": "Certo, então você tem que escolher qual caminho quer seguir:",
-        "images": ["./assets/images/scene02-forest.png", "./assets/images/scene02-mountain.jpg"],
+        "text": "Certo! Então escolha qual caminho deseja seguir:",
+        "image": "./assets/images/scene02.png",
         "options": [
             {
                 "text": "O caminho da floresta parece legal!",
@@ -28,8 +28,8 @@ const scenes = {
         ]
     },
     3: {
-        "text": "No meio da floresta você encontra uma velha bruxa. Que te diz: Olá jovem aventureiro, aceita essa maça nada suspeita?”",
-        "images": ["./assets/images/scene03.jpg"],
+        "text": 'No meio da floresta você encontra uma velha bruxa. Que te diz: "Olá jovem aventureiro, aceita essa maçã nada suspeita?"',
+        "image": "./assets/images/scene03.jpg",
         "options": [
             {
                 "text": "Claro, não parece ser uma má ídeia!",
@@ -43,7 +43,7 @@ const scenes = {
     },
     4: {
         "text": "Enquanto subia a montanha você avista um pobre cão perdido e com frio.”",
-        "images": ["./assets/images/scene04.jpg"],
+        "image": "./assets/images/scene04.jpg",
         "options": [
             {
                 "text": "Acho que devo ajuda-lo!",
@@ -56,8 +56,8 @@ const scenes = {
         ]
     },
     5: {
-        "text": "Após uma longa caminhada você consegue sair da floresta, e se depara com um lago. Você tem duas opções: cruzar a ponte completmante estável ou pegar um barquinho todo fudido.",
-        "images": ["./assets/images/scene05.avif"],
+        "text": "Após uma longa caminhada você consegue sair da floresta, e se depara com um lago. Você tem duas opções: cruzar a ponte estável ou pegar um barquinho todo fudido.",
+        "image": "./assets/images/scene05.png",
         "options": [
             {
                 "text": "Irei pela ponte.",
@@ -71,7 +71,7 @@ const scenes = {
     },
     6: {
         "text": "Alguns metros acima, na montanha, surge uma alcateia de lobos raivosos.",
-        "images": ["./assets/images/scene06.jpg"],
+        "image": "./assets/images/scene06.jpg",
         "options": [
             {
                 "text": "Eu jamais correrei de uma briga!",
@@ -84,27 +84,32 @@ const scenes = {
         ]
     },
     7: {
-        "text": "O barco afunda e você morre afogado! O barco estava fudido, que escolha imbecil!",
-        "images": ["./assets/images/scene07.jpg"],
-        "options": []
+        "text": "O barco afundou e você morreu afogado! O barco estava todo fudido, que escolha imbecil!",
+        "image": "./assets/images/scene07.jpg",
+        "options": [
+            {
+                "text": "Tentar novamente",
+                "toScene": 1
+            }
+        ]
     },
     8: {
         "text": "Após atravessar a ponte derepente surge um grande ogro, que te acerta e arremessa sua arma no lago, O que você fará?",
-        "images": ["./assets/images/scene08.jpg"],
+        "image": "./assets/images/scene08.jpg",
         "options": [
             {
                 "text": "**Aceito a morte e me mijo todo**",
                 "toScene": 13
             },
             {
-                "text": "**Aremessa a maça**",
+                "text": "**Aremesso a maça**",
                 "toScene": 10
             }
         ]
     },
     9: {
         "text": "Um dos lobos morde sua mão e sua arma vai montanha abaixo. O que você fará?",
-        "images": ["./assets/images/scene09.jpg"],
+        "image": "./assets/images/scene09.jpg",
         "options": [
             {
                 "text": "**Aceito a morte e me mijo todo**",
@@ -118,28 +123,53 @@ const scenes = {
     },
     10: {
         "text": "Você acerta no meio da testa do ogro e ele caí morto. Assim você termina sua primeira aventura!",
-        "images": ["./assets/images/scene10.jpg"],
-        "options": []
+        "image": "./assets/images/scene10.jpg",
+        "options": [
+            {
+                "text": "Tentar novamente",
+                "toScene": 1
+            }
+        ]
     },
     11: {
         "text": "Surge um cão e espanta uma fodendo alcateia de lobos. Assim você termina sua primeira aventura!",
-        "images": ["./assets/images/scene11.jpg"],
-        "options": []
+        "image": "./assets/images/scene11.jpg",
+        "options": [
+            {
+                "text": "Tentar novamente",
+                "toScene": 1
+            }
+        ]
     },
     12: {
         "text": "Ao tentar bater em fuga você tropeça e caí da montanha, assim morendo. Que aventureiro covarde!",
-        "images": ["./assets/images/scene12.jpg"],
-        "options": []
+        "image": "./assets/images/scene12and13.jpg",
+        "options": [
+            {
+                "text": "Tentar novamente",
+                "toScene": 1
+            }
+        ]
     },
     13: {
         "text": "Era melhor ter ficado em casa!",
-        "images": ["./assets/images/scene13.jpg"],
-        "options": []
+        "image": "./assets/images/scene12and13.jpg",
+        "options": [
+            {
+                "text": "Tentar novamente",
+                "toScene": 1
+            }
+        ]
     },
     14: {
         "text": "É sério? Então até depois!",
-        "images": ["./assets/images/scene14.jpg"],
-        "options": []
+        "image": "./assets/images/scene14.jpg",
+        "options": [
+            {
+                "text": "Tentar novamente",
+                "toScene": 1
+            }
+        ]
     },
 }
 
--- theAdventure/src/index.js
+++ theAdventure/src/index.js
@@ -1,36 +1,47 @@
 import scenes from './assets/scenes.js';
 
 window.addEventListener("load", init);
+const root = document.querySelector(`#root`);
+
+const conditions = {
+    'pickedApple' : false,
+    'salvedDog' : false
+}
 
 function init() {
 
     loadScene(1)
 
+    for (let i = 1; i <= 2; i++) {
+        addButtonEvent(i)
+    }
+
 }
 
 function loadScene(toScene) {
 
-    const scene = scenes[toScene];
-    let i = 1;
+    root.setAttribute('actualScene', toScene) 
 
-    for (let img of scene.images) {
-        changeImage(img, i);
-        i++;
-    }
+    const scene = scenes[toScene];
+
+    changeImage(scene.image);
 
     changeText(scene.text);
-    i = 1
 
-    for (let option of scene.options) {
-        changeButtonText({...option, i})
-        i++;
+    for (let i = 1; i <= 2; i++) {
+        let option = scene.options[i - 1] || { text: null, toScene: null }
+        if((root.getAttribute('actualScene') == 8 && conditions.pickedApple == false && i == 2) || (root.getAttribute('actualScene') == 9 && conditions.salvedDog == false && i == 2)){
+            option = { text: null, toScene: null }
+        }  
+        changeButtonProps({ ...option, i })
+
     }
 
 }
 
-function changeImage (image, i) {
+function changeImage(image) {
 
-    const imageEl = document.querySelector(`#image-${i}`);
+    const imageEl = document.querySelector(`#image`);
     imageEl.src = image;
 
 }
@@ -40,13 +51,35 @@ function changeText(text) {
     p.textContent = text;
 }
 
-function changeButtonText({ text, toScene, i }) {
+function changeButtonProps({ text, toScene, i }) {
 
     const button = document.querySelector(`#option-${i}`);
 
-    button.textContent = text;
+    if (text == null && toScene == null) {
+        button.className = 'disabled';
+    } else {
+        button.className = 'enabled'
+        button.textContent = text;
+        
+        button.setAttribute('toScene', toScene)
+
+    }
+}
+
+function addButtonEvent(i) {
+
+    const button = document.querySelector(`#option-${i}`);
+
     button.addEventListener('click', () => {
-        loadScene(toScene)
+
+        const toScene = button.getAttribute('toScene');
+        const actualScene = root.getAttribute('actualScene');
+        const clickedButton = button.getAttribute('value');
+
+        if(actualScene == 3 && clickedButton == 1) conditions.pickedApple = true;
+        if(actualScene == 4 && clickedButton == 1) conditions.salvedDog = true;
+
+        loadScene(toScene);
     })
 
 }
